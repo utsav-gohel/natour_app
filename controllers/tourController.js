6a768ffc314653ad90679dd5dd390a84baaa65d8
utsavgohel-simform <utsav@simformsolutions.com>
2023-03-10 13:13:24 +0530

    Implemented middleware checkId and checkbody

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,27 @@ const fs = require("fs");
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../Dev-data/tour-simple.json`)
 );
+
+//This is middleware that created to check whether requested tour id is valid or not..
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+  if (req.params.id * 1 >= tours.length) {
+    return res.status(404).json({ status: "fail", message: "Invalid ID" });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  // console.log(`body is ${val}`);
+  console.log(req.body.name, req.body.price);
+  if (!req.body.name || !req.body.price) {
+    return res
+      .status(404)
+      .json({ status: "fail", message: "Please enter correct name and price" });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -51,17 +72,9 @@ exports.CreateNewTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 >= tours.length) {
-    res.status(404).json({ status: "fail", message: "Invalid ID" });
-  } else {
-    res.status(200).json({ status: "success", message: "Updated" });
-  }
+  res.status(200).json({ status: "success", message: "Updated" });
 };
 
 exports.deletTourById = (req, res) => {
-  if (req.params.id * 1 >= tours.length) {
-    res.status(404).json({ status: "fail", message: "Invalid ID" });
-  } else {
-    res.status(200).json({ status: "Delete success", data: null });
-  }
+  res.status(200).json({ status: "Delete success", data: null });
 };
