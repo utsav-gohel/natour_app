8775fe2097212795d0d34d7b8562ff7741bfb9a6
utsavgohel-simform <utsav@simformsolutions.com>
2023-03-10 11:11:44 +0530

    Implemented Controller File Structure for UserRoutes

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is not defined yet",
+  });
+};
+exports.CreateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is not defined yet",
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is not defined yet",
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is not defined yet",
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route is not defined yet",
+  });
+};
