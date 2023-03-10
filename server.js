149ecfe9812bfbabc1f94600c29503e63e90c43b
utsavgohel-simform <utsav@simformsolutions.com>
2023-03-10 17:02:43 +0530

    Setup of Databse(MongoDB)

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
+
 const app = require("./index");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log("Connected with Database"));
+
+// console.log(process.env);
+PORT = process.env.PORT || 3000;
+
 //SERVER
-app.listen(3000, () => {
-  console.log("Server Started");
+app.listen(PORT, () => {
+  console.log("Server Started on ", PORT);
 });
