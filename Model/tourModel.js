3a6c052a766006ee17795ca5fc0880d67d40f38c
utsavgohel-simform <utsav@simformsolutions.com>
2023-03-14 10:33:01 +0530

    Added important fields to Model

--- Model/tourModel.js
+++ Model/tourModel.js
@@ -2,17 +2,54 @@ const mongoose = require("mongoose");
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, "A tour must have a name"],
+    required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "A tour must have duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a Group Size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have difiiculty"],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    require: [true, "A tour must have a price"],
+    required: [true, "A tour must have a price"],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, //this will remove white space from summary
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String], //this will create an array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date], //this will create an array of different dates
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
