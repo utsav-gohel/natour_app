f0fcda6e7134b9b8ce276a3bebe11e79ec6bc396
utsavgohel-simform <utsav@simformsolutions.com>
2023-03-14 10:31:44 +0530

    Implemented Sorting Feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,12 +2,37 @@ const Tour = require("../Model/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const allTour = await Tour.find();
+    //Build query
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //1B)Advance filtering for less than and greter than
+    let querystr = JSON.stringify(queryObj);
+    querystr = querystr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(querystr));
+    let query = Tour.find(JSON.parse(querystr));
+
+    // 2)Sorting
+    //remaining to do from 97. at 6 min
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      // default mode
+      query = query.sort("-cratedAt");
+    }
+
+    // 3)Field limiting  //by this user will select which field want to show in response
+
+    //Execute query
+    const tours = await query;
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
-      resultCount: allTour.length,
-      data: { allTour },
+      resultCount: tours.length,
+      data: { tours },
     });
   } catch (err) {
     res.status(400).json({
@@ -47,7 +72,8 @@ exports.CreateNewTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: "fail",
-      message: "Invalid data sent",
+      // message: "Invalid data sent",
+      message: err,
     });
   }
 };
